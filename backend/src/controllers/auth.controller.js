aaea6339c45aabc42504120a648efa45e16a0903
Marshal Mandi <mandimohananand@gmail.com>
2025-06-01 19:47:48 +0530

    added routes structure, database and streaming platform

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export let signup = async (req, res) => {
+    res.send("signup route")
+}
+
+export let login = async (req, res) => {
+    res.send("login route")
+}
+
+export let logout = async (req, res) => {
+    res.send("logout route")
+}
